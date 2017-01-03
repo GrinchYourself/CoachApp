b8ec0ae27ad9d08208d94be648a83ad7e7c121d2
GrinchYourself <delaporteol@me.com>
2017-01-03 15:00:48 +0100

    Display List of Players Refresh list of players after the add of a player

--- Coach/PlayersViewController.swift
+++ Coach/PlayersViewController.swift
@@ -13,20 +13,26 @@ class PlayersViewController: UITableViewController {
     
     //MARK: - VC's Variables
     let realm = try! Realm()
-    var players : Results<Player>?
+    var token: NotificationToken?
+    var players : Results<Player>{
+        get {
+            return realm.objects(Player.self).sorted(byProperty: "name")
+        }
+    }
 
+    //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         print("Players")
-        players = realm.objects(Player.self).sorted(byProperty: "name")
+        // Do any additional setup after loading the view, typically from a nib.
+        title = "Players"
         
+        token = players.addNotificationBlock { (changes:RealmCollectionChange) in
+            self.tableView.reloadData()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    //MARK: - TableView Methods
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -34,19 +40,17 @@ class PlayersViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return players!.count
+        return players.count
     }
     
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//            //iToPlay += 1
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Toto", for: indexPath)
-//            // Configure the cell...
-//        //let task = tasks![1]
-//        let index = Int(indexPath)
-//        let player = players![index]
-//        //cell.textLabel = player.
-//            return cell
-//        
-//    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Toto", for: indexPath)
+
+        cell.textLabel?.text = players[indexPath.row].name
+        cell.detailTextLabel?.text = players[indexPath.row].phoneNumber
+        return cell
+        
+    }
 }
 
